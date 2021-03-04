8b18a8c92a5593790826e780db8793db801015d7
PurushothamVadde <purushotham.vadde93@gmail.com>
2021-03-03 19:34:20 -0600

    Added business URL category list csv files

--- Webscraping/Business_URL/Yelp_Business_URL.py
+++ Webscraping/Business_URL/Yelp_Business_URL.py
@@ -6,10 +6,11 @@ import time
 
 
 # Function to extract the URL of pages
-def Extracting_URL(mainpage,links):
-    URL = mainpage
+def Extracting_URL(main_url, links):
+    URL = main_url
     # initiating the webdriver. Parameter includes the path of the webdriver.
     driver = webdriver.Chrome('C:/Users/Prudhvi/Anaconda3/chromedriver')
+    # time.sleep(5)
     driver.get(URL)
     # this is just to ensure that the page is loaded
     time.sleep(3)
@@ -20,8 +21,9 @@ def Extracting_URL(mainpage,links):
     tags = soup('a', href=True)
     for tag in tags:
         if "start" in tag["href"]:
-            if (tag['href'] not in links) and ('login' not in tag['href']) and ('signup'not in tag['href']):
-                # print(tag['href'])
+            if ('?find_desc=' in tag['href'] ) and (tag['href'] not in links) and ('login' not in tag['href']) and ('signup'not in tag['href'])and ('biz' not in tag['href']):
+            # if (tag['href'] not in links) and ('login' not in tag['href']) and ('signup'not in tag['href']) and ('biz' not in tag['href']):
+            #     print(tag['href'])
                 links.append(tag['href'])
     return links
 
@@ -30,6 +32,7 @@ def Extracting_Business_URL(main_url, base_url, Business_links):
     URL = main_url
     # initiating the webdriver. Parameter includes the path of the webdriver.
     driver = webdriver.Chrome('C:/Users/Prudhvi/Anaconda3/chromedriver')
+    # time.sleep(5)
     driver.get(URL)
     # this is just to ensure that the page is loaded
     time.sleep(3)
@@ -39,13 +42,13 @@ def Extracting_Business_URL(main_url, base_url, Business_links):
     tags = soup.find_all("a", class_="css-166la90")
     #     print(tags)
     for tag in tags:
-        if ("osq=Restaurants" in tag["href"]) and (base_url + tag['href'] not in Business_links):
+        if ("osq" in tag["href"]) and (base_url + tag['href'] not in Business_links):
             Business_links.append(base_url + tag['href'])
 
     return Business_links
 
 
-def main(base_url,main_url):
+def main(base_url,main_url,num):
     # list to store the links of pages with business list
     links = []
     # list to store the list of business url
@@ -53,25 +56,35 @@ def main(base_url,main_url):
     # adding the first page url with business list
     links.append(main_url + '&start=0')
     # Extracting all page urls with business list
-    for i in range(1, 10):
+    for i in range(1, 6):
         links = Extracting_URL(main_url, links)
         url = links[-1]
         main_url = url
-    # links
+    print(links)
     # Extracting all Business URL from all pages
     for link in links:
         Extracting_Business_URL(link, base_url, Business_links)
     # Saving all the Business URL to csv file
     df = pd.DataFrame({'Business_links': Business_links})
-    df.to_csv('Business_links.csv')
-
+    df.to_csv(f'Business_links{num}.csv')
+    print(len(Business_links))
     return Business_links
 
 
 if __name__== '__main__':
 
+    df = pd.read_csv('category _list.csv')
+    Categories = df['category']
+    Categories = Categories.tolist()
+
+    for num in range(33,len(Categories),1):
     # main_url is the intial search page url with results
-    main_url = 'https://www.yelp.com/search?find_desc=Restaurants&find_loc=Seattle%2C%20WA'
+    # main_url = 'https://www.yelp.com/search?find_desc=HVAC&find_loc=Seattle%2C%20WA'
+        main_url = Categories[num]
     # Base yelp url
-    base_url = 'https://www.yelp.com'
-    main(base_url,main_url)
+        print(main_url)
+        base_url = 'https://www.yelp.com'
+        main(base_url,main_url,num)
+
+
+#https://www.yelp.com/search?find_desc=Restaurants&find_loc=Seattle%2C%20WA
